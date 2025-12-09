9d3aa8020ba339290119387d5ba789b405be9d5e
Aaron Wright <aarnwri@gmail.com>
2025-12-08 19:39:48 -0800

    Move VERSION declaration

--- lib/console/canvas.rb
+++ lib/console/canvas.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-require_relative "canvas/version"
 require_relative "canvas/layer"
 
 module Console
   module Canvas
+    VERSION = "0.1.0"
+
     class Error < StandardError; end
     # Your code goes here...
   end
--- lib/console/canvas/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Console
-  module Canvas
-    VERSION = "0.1.0"
-  end
-end
