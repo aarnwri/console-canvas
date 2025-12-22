3d2a8b5ab1dd65b6aac3012f7bc628197a0b4df0
Aaron Wright <aarnwri@gmail.com>
2025-12-22 07:05:39 -0800

    Remove useless test

--- spec/console/canvas_spec.rb
+++ spec/console/canvas_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Console::Canvas do
   it "has a version number" do
     expect(Console::Canvas::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
