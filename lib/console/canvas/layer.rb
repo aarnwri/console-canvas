69fbee410f1f828fe7923508614b4070b64f6255
Aaron Wright <aarnwri@gmail.com>
2025-12-22 20:42:11 -0800

    Add layer content

--- lib/console/canvas/layer.rb
+++ lib/console/canvas/layer.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
 require "mface"
+require "io/console"
 
 module Console
   module Canvas
     class Layer
+      MAX_WIDTH = IO.console.winsize[1]
+      DEFAULT_CHAR = " "
+
       # Creates a new layer object
       # size_x: size of the layer on screen moving left to right
       # size_y: size of the layer on screen moving top to bottom
       # def_char: the default character to use
+      #
       # @grid: 2d array used to store the contents of the layer
-      def initialize(size_x: 0, size_y: 0, def_char: " ")
+      # @def_char: var for storing def_char in case it's modified
+      def initialize(size_x = 0, size_y = 0, def_char = DEFAULT_CHAR)
         Mface.req_int(:size_x, size_x)
         Mface.req_int(:size_y, size_y)
         Mface.req_char(:def_char, def_char)
@@ -29,6 +35,66 @@ module Console
       # Get the size of the @grid in the y direction, top to bottom
       def size_y
         @grid.count
+      end
+
+      # Returns true if there is no meaningful content in @grid
+      def empty?
+        size_x == 0 || size_y == 0
+      end
+
+      # Inserts the given str at the given location in the @grid
+      def insert_str(str, loc = Canvas::Loc.new)
+        unless sufficient_for_str?(str, loc)
+          expand_for_str(str, loc)
+        end
+
+        @grid[loc.y][loc.x..(loc.x + str.length - 1)] = str.chars
+      end
+
+      def sufficient_for_str?(str, loc = Canvas::Loc.new)
+        return false if empty?
+
+        start_loc = Canvas::Loc.new(loc.x, loc.y)
+        end_loc = Canvas::Loc.new(start_loc.x + str.length - 1, loc.y)
+
+        return false unless size_y > end_loc.y
+        return false unless size_x > end_loc.x
+        true
+      end
+
+      def expand_for_str(str, loc = Canvas::Loc.new)
+        start_loc = Canvas::Loc.new(loc.x, loc.y)
+        end_loc = Canvas::Loc.new(start_loc.x + str.length - 1, start_loc.y)
+
+        # Make sure end_loc is still on screen
+        raise Canvas::Error, "location off screen" if loc_off_screen?(end_loc)
+
+        if end_loc.y >= size_y
+          diff = end_loc.y - size_y
+          needed = diff + 1
+          add_row(needed)
+        end
+
+        if end_loc.x >= size_x
+          diff = end_loc.x - size_x
+          needed = diff + 1
+          add_col(needed)
+        end
+      end
+
+      def add_row(num = 1)
+        num.times { @grid << Array.new(size_x) { DEFAULT_CHAR } }
+      end
+
+      def add_col(num = 1)
+        @grid.map! { |row| row + Array.new(num) { DEFAULT_CHAR } }
+      end
+
+      private
+
+      def loc_off_screen?(loc)
+        count_at_loc = loc.x + 1
+        count_at_loc > MAX_WIDTH
       end
     end
   end
---
+++ spec/console/canvas/layer_spec.rb
@@ -0,0 +1,179 @@
+# frozen_string_literal: true
+
+RSpec.describe Console::Canvas::Layer do
+  subject(:layer) { described_class.new }
+  it { is_expected.to respond_to(:size_x) }
+  it { is_expected.to respond_to(:size_y) }
+  it { is_expected.to respond_to(:empty?) }
+  it { is_expected.to respond_to(:insert_str) }
+  it { is_expected.to respond_to(:sufficient_for_str?) }
+  it { is_expected.to respond_to(:expand_for_str) }
+
+  describe "initialization" do
+    it "uses sensible defaults" do
+      expect(layer.instance_variable_get(:@grid).count).to eq(0)
+      expect(layer.instance_variable_get(:@def_char)).to eq(" ")
+    end
+    it "raises an error if the given x or y value is not an integer" do
+      expect { described_class.new(3.4, 2) }.to raise_error(ArgumentError)
+      expect { described_class.new(2, 3.4) }.to raise_error(ArgumentError)
+    end
+    it "raises an error if the given default char is not a char" do
+      expect { described_class.new(0, 0, 5) }.to raise_error(ArgumentError)
+      expect { described_class.new(0, 0, "testing") }.to raise_error(ArgumentError)
+    end
+    it "sets up the grid with the default char" do
+      layer = described_class.new(1, 1, "j")
+      expect(layer.instance_variable_get(:@grid)[0][0]).to eq("j")
+    end
+  end
+
+  describe "#size_x" do
+    let(:layer) { described_class.new(2, 1) }
+    it "returns the size of the grid in the x direction" do
+      expect(layer.size_x).to eq(2)
+    end
+    context "when grid is empty (nothing in the y direction)" do
+      let(:layer) { described_class.new }
+      it "does not error out, but returns 0" do
+        expect(layer.size_x).to eq(0)
+      end
+    end
+  end
+
+  describe "#size_y" do
+    let(:layer) { described_class.new(2, 1) }
+    it "returns the size of the grid in the y direction" do
+      expect(layer.size_y).to eq(1)
+    end
+  end
+
+  describe "#empty?" do
+    context "with an empty grid" do
+      let(:layer) { described_class.new }
+      it "returns true" do
+        expect(layer.empty?).to be true
+      end
+    end
+    context "with a non-empty grid" do
+      let(:layer) { described_class.new(2, 1) }
+      it "returns false" do
+        expect(layer.empty?).to be false
+      end
+    end
+  end
+
+  describe "#insert_str(str, loc = Console::Canvas::Loc.new)" do
+    context "when the layer is big enough" do
+      let(:layer) { described_class.new(6, 1) }
+      let(:str) { "fooey" }
+      it "inserts the given string into the grid at the given location" do
+        layer.insert_str(str, Console::Canvas::Loc.new(1, 0))
+        expect(layer.instance_variable_get(:@grid)).to eq([[" ", "f", "o", "o", "e", "y"]])
+      end
+    end
+    context "when the layer is not big enough" do
+      let(:layer) { described_class.new(4, 1) }
+      let(:str) { "fooey" }
+      it "expands the layer, and then inserts the string as normal" do
+        layer.insert_str(str, Console::Canvas::Loc.new(1, 0))
+        expect(layer.instance_variable_get(:@grid)).to eq([[" ", "f", "o", "o", "e", "y"]])
+      end
+    end
+    context "when the layer is not big enough in the y direction" do
+      let(:layer) { described_class.new(4, 1) }
+      let(:str) { "fooey" }
+      it "expands the layer, and then inserts the string as normal" do
+        layer.insert_str(str, Console::Canvas::Loc.new(1, 1))
+        expect(layer.instance_variable_get(:@grid)).to eq([
+          [" ", " ", " ", " ", " ", " "],
+          [" ", "f", "o", "o", "e", "y"]
+        ])
+      end
+    end
+  end
+
+  describe "#sufficient_for_str?" do
+    context "when layer is big enough for string at location" do
+      let(:layer) { described_class.new(2, 1) }
+      let(:str) { "fo" }
+      it "returns true" do
+        expect(layer.sufficient_for_str?(str)).to be true
+      end
+    end
+    context "when layer is not big enough for string at location" do
+      let(:layer) { described_class.new(2, 1) }
+      let(:str) { "fooey" }
+      it "returns false" do
+        expect(layer.sufficient_for_str?(str)).to be false
+      end
+    end
+  end
+
+  describe "#expand_for_str" do
+    context "when layer is too small in the x direction" do
+      let(:layer) { described_class.new(2, 1) }
+      let(:str) { "fooey" }
+      it "expands the grid in the x direction" do
+        layer.expand_for_str(str, Console::Canvas::Loc.new(1, 0))
+        grid = layer.instance_variable_get(:@grid)
+        expect(grid[0].length).to eq(6)
+        expect(grid.length).to eq(1)
+      end
+    end
+    context "when layer is too small in the y direction" do
+      let(:layer) { described_class.new(2, 1) }
+      let(:str) { "fooey" }
+      it "expands the grid in the y direction" do
+        layer.expand_for_str(str, Console::Canvas::Loc.new(1, 1))
+        grid = layer.instance_variable_get(:@grid)
+        expect(grid[0].length).to eq(6)
+        expect(grid.length).to eq(2)
+        expect(grid[0]).to eq([" ", " ", " ", " ", " ", " "])
+        expect(grid[1]).to eq([" ", " ", " ", " ", " ", " "])
+      end
+    end
+    context "when the given location is off the screen" do
+      let(:off_screen_loc) { Console::Canvas::Loc.new(1000, 1) }
+      it "raises an error indicating that the location is off the screen" do
+        expect { layer.expand_for_str("fooey", off_screen_loc) }.to raise_error(Console::Canvas::Error, /location off screen/)
+      end
+    end
+  end
+
+  describe "#add_row" do
+    it "defaults to one row" do
+      layer.add_row
+      expect(layer.instance_variable_get(:@grid).count).to eq(1)
+    end
+    it "adds the number of rows specified" do
+      layer.add_row(2)
+      expect(layer.instance_variable_get(:@grid).count).to eq(2)
+    end
+    context "when the width of the rows is greater than 0" do
+      let(:layer) { described_class.new(2, 1) }
+
+      it "adds the row such that it is the same width as all the other rows" do
+        layer.add_row
+        grid = layer.instance_variable_get(:@grid)
+        expect(grid.last.count).to eq(grid.first.count)
+      end
+    end
+  end
+
+  describe "#add_col" do
+    it "defaults to one column" do
+      # NOTE: We have to add a row first, otherwise, there is nothing to add
+      # a column to...
+      layer.add_row
+      layer.add_col
+      expect(layer.instance_variable_get(:@grid)[0].count).to eq(1)
+    end
+    it "adds the number of columns specified to each exesting row" do
+      layer.add_row(2)
+      layer.add_col(2)
+      expect(layer.instance_variable_get(:@grid)[0].count).to eq(2)
+      expect(layer.instance_variable_get(:@grid)[1].count).to eq(2)
+    end
+  end
+end
